71f8fc17119abf51c25e5969d5f8ac9604666686
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2025-04-08 10:38:08 +0530

    Create same_tree.c

---
+++ same_tree.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p==q) return true;
+    if (!p || !q || p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);    
+}
