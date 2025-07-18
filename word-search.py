8513a04e2855e0c31f93de1d6533db0a28aee67d
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2025-07-18 16:48:45 +0530

    Update word-search.py

--- word-search.py
+++ word-search.py
@@ -4,22 +4,22 @@ class Solution:
         n = len(board[0])
 
         def dfs(i: int, j: int, s: int) -> bool:
-        if i < 0 or i == m or j < 0 or j == n:
-            return False
-        if board[i][j] != word[s] or board[i][j] == '*':
-            return False
-        if s == len(word) - 1:
-            return True
+            if i < 0 or i == m or j < 0 or j == n:
+                return False
+            if board[i][j] != word[s] or board[i][j] == '*':
+                return False
+            if s == len(word) - 1:
+                return True
 
-        cache = board[i][j]
-        board[i][j] = '*'
-        isExist = (dfs(i + 1, j, s + 1) or
-                    dfs(i - 1, j, s + 1) or
-                    dfs(i, j + 1, s + 1) or
-                    dfs(i, j - 1, s + 1))
-        board[i][j] = cache
+            cache = board[i][j]
+            board[i][j] = '*'
+            isExist = (dfs(i + 1, j, s + 1) or
+                        dfs(i - 1, j, s + 1) or
+                        dfs(i, j + 1, s + 1) or
+                        dfs(i, j - 1, s + 1))
+            board[i][j] = cache
 
-        return isExist
+            return isExist
 
         return any(dfs(i, j, 0)
                 for i in range(m)
