9bd9123014b1b798c7bfe33c609fe0fc1eb47827
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2025-07-24 06:56:23 +0530

    Update fibonacci-number.java

--- fibonacci-number.java
+++ fibonacci-number.java
@@ -1,6 +1,18 @@
 class Solution {
-    public int fib(int n) {
+    public int fib1(int n) { // recursive edition
         if (n <= 1) return n;
         return fib(n - 1) + fib(n - 2);        
+    }
+    public int fib(int n) { // faster loop edition
+        if ( n==0 ) return 0;
+        int currentNumber = 1;
+        int prevNumber = 0;
+        int temp;
+        for (int i=2; i<=n; i++) {
+            temp = prevNumber + currentNumber;
+            prevNumber = currentNumber;
+            currentNumber = temp;
+        }
+        return currentNumber;
     }
 }
